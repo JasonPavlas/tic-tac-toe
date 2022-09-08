066b9a9595466c879e3123313ea1fd10a25a6e32
Jason Pavlas <jasonpavlas@jasons-mbp.lan>
2022-09-07 22:26:19 -0400

    adding some code

---
+++ testpy.py
@@ -0,0 +1,47 @@
+# ------------------------------------------------------------------
+# Author: Jason Pavlas
+# Created: 7 September,2022, 10:05 PM
+# Email: jpavlas@jaxitp.com
+# ------------------------------------------------------------------
+# OG
+# Author: aqeelanwar
+# Created: 12 March,2020, 7:06 PM
+# Email: aqeel.anwar@gatech.edu
+
+from tkinter import *
+import numpy as np
+
+size_of_board = 600
+symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
+symbol_thickness = 50
+symbol_X_color = '#EE4035'
+symbol_O_color = '#0492CF'
+Green_color = '#7BC043'
+
+
+class Tic_Tac_Toe():
+    # ------------------------------------------------------------------
+    # Initialization Functions:
+    # ------------------------------------------------------------------
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Tic-Tac-Toe')
+        self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
+        self.canvas.pack()
+        # Input from user in form of clicks
+        self.window.bind('<Button-1>', self.click)
+
+        self.initialize_board()
+        self.player_X_turns = True
+        self.board_status = np.zeros(shape=(3, 3))
+
+        self.player_X_starts = True
+        self.reset_board = False
+        self.gameover = False
+        self.tie = False
+        self.X_wins = False
+        self.O_wins = False
+
+        self.X_score = 0
+        self.O_score = 0
+        self.tie_score = 0
